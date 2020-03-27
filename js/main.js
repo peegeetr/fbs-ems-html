acb27a68424e33a367bbd4d2595b876712f3d4d9
Vincent Ramirez <vincent.ramirez@frontlinebusiness.com.ph>
2020-03-27 14:58:00 +0800

    Added Sliding Notification

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
-var trigger = document.querySelector(".nav-trigger");
-var mobileNav = document.querySelector(".side-nav");
+var trigger = document.querySelector('.nav-trigger');
+var mobileNav = document.querySelector('.side-nav');
 
-trigger.addEventListener("click",function(event){
+trigger.addEventListener('click', function(event) {
   event.preventDefault();
   mobileNav.classList.toggle('show');
 });
 
+const btnTrigger = document.querySelector('.btnTrigger');
+const notification = document.querySelector('.small-notif-notification');
+const close = document.querySelector('.small-notif-close');
 
-    
+btnTrigger.addEventListener('click', e => {
+  e.preventDefault();
+  // console.log(btnTrigger);
+  if (!notification.classList.contains('small-notif-show')) {
+    notification.classList.add('small-notif-show');
+    setTimeout(() => {
+      notification.classList.remove('small-notif-show');
+    }, 3000);
+  }
+});
+
+close.addEventListener('click', e => {
+  e.preventDefault();
+  notification.classList.remove('small-notif-show');
+});
